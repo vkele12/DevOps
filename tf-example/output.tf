977a14548eb8cf174b415108d9e273b7cfb436ed
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-12 14:24:41 +0530

    Update output.tf

--- tf-example/output.tf
+++ tf-example/output.tf
@@ -1,7 +1,7 @@
 output "deployed_role_id" {
-  value = aws_iam_role.plx-user-management-deployment-roles.id
+  value = aws_iam_role.${var.vishwa_roles}.id
 }
 
 output "deployed_role_arn" {
-  value = aws_iam_role.plx-user-management-deployment-roles.arn
+  value = aws_iam_role.${var.vishwa_roles}.arn
 }
